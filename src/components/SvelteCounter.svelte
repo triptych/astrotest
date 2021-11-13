496c0f18bc9ab4cfaa767001d842875acc61e818
Andrew Wooldridge <triptych@gmail.com>
2021-11-12 20:04:34 -0800

    testing making changes

--- src/components/SvelteCounter.svelte
+++ src/components/SvelteCounter.svelte
@@ -8,10 +8,16 @@
   function subtract() {
     count -= 1;
   }
+
+  function reset() {
+    count = 0;
+  }
 </script>
 
 <div id="svelte" class="counter">
   <button on:click={subtract}>-</button>
   <pre>{ count }</pre>
   <button on:click={add}>+</button>
+  <br/>
+  <button on:click={reset}>~</button>
 </div>
